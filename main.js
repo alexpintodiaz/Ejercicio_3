d528a25a62fb331dbdf0f6ed68fc67cc0a9de62d
alexpintodiaz <alexanderp143@hotmail.com>
2021-04-21 19:04:18 -0500

    Taller_3.3

---
+++ main.js
@@ -0,0 +1,4 @@
+let A = parseInt(prompt("Por favor ingrese un primer número"))
+let B = parseInt(prompt("Por favor ingrese un segundo número"))
+let Res = (A + B)
+console.log("La suma de los numeros es = ", Res)
